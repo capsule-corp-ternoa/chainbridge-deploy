ec26912677bd66d56569ef619d0a46fdc22132cf
Timothée Delabrouille <timothee@capsule-corp.io>
2022-02-07 16:52:02 +0100

    get rid of erc721 centrifuge and generic

--- cb-sol-cli/cmd/admin.js
+++ cb-sol-cli/cmd/admin.js
@@ -1,44 +1,44 @@
 const ethers = require('ethers');
 const constants = require('../constants');
 
-const {Command} = require('commander');
-const {setupParentArgs, waitForTx, log} = require("./utils")
+const { Command } = require('commander');
+const { setupParentArgs, waitForTx, log } = require("./utils")
 
 const isRelayerCmd = new Command("is-relayer")
     .description("Check if address is relayer")
     .option('--relayer <value>', 'Address to check', constants.relayerAddresses[0])
     .option('--bridge <address>', 'Bridge contract address', constants.BRIDGE_ADDRESS)
     .action(async function (args) {
-            await setupParentArgs(args, args.parent.parent)
-            const bridgeInstance = new ethers.Contract(args.bridge, constants.ContractABIs.Bridge.abi, args.wallet);
+        await setupParentArgs(args, args.parent.parent)
+        const bridgeInstance = new ethers.Contract(args.bridge, constants.ContractABIs.Bridge.abi, args.wallet);
 
-            let res = await bridgeInstance.isRelayer(args.relayer)
-            console.log(`[${args._name}] Address ${args.relayer} ${res ? "is" : "is not"} a relayer.`)
+        let res = await bridgeInstance.isRelayer(args.relayer)
+        console.log(`[${args._name}] Address ${args.relayer} ${res ? "is" : "is not"} a relayer.`)
     })
 
 const addAdminCmd = new Command("add-admin")
-  .description("Adds an admin")
-  .option('--admin <address>', 'Address of admin', constants.adminAddresses[0])
-  .option('--bridge <address>', 'Bridge contract address', constants.BRIDGE_ADDRESS)
-  .action(async function (args) {
-    await setupParentArgs(args, args.parent.parent)
-    const bridgeInstance = new ethers.Contract(args.bridge, constants.ContractABIs.Bridge.abi, args.wallet);
-    log(args, `Adding ${args.admin} as a admin.`)
-    let tx = await bridgeInstance.grantRole(constants.ADMIN_ROLE, args.admin)
-    await waitForTx(args.provider, tx.hash)
-  })
+    .description("Adds an admin")
+    .option('--admin <address>', 'Address of admin', constants.adminAddresses[0])
+    .option('--bridge <address>', 'Bridge contract address', constants.BRIDGE_ADDRESS)
+    .action(async function (args) {
+        await setupParentArgs(args, args.parent.parent)
+        const bridgeInstance = new ethers.Contract(args.bridge, constants.ContractABIs.Bridge.abi, args.wallet);
+        log(args, `Adding ${args.admin} as a admin.`)
+        let tx = await bridgeInstance.grantRole(constants.ADMIN_ROLE, args.admin)
+        await waitForTx(args.provider, tx.hash)
+    })
 
 const removeAdminCmd = new Command("remove-admin")
-  .description("Removes an admin")
-  .option('--admin <address>', 'Address of admin', constants.adminAddresses[0])
-  .option('--bridge <address>', 'Bridge contract address', constants.BRIDGE_ADDRESS)
-  .action(async function (args) {
-    await setupParentArgs(args, args.parent.parent)
-    const bridgeInstance = new ethers.Contract(args.bridge, constants.ContractABIs.Bridge.abi, args.wallet);
-    log(args, `Removing ${args.admin} as a admin.`)
-    let tx = await bridgeInstance.revokeRole(constants.ADMIN_ROLE, args.admin)
-    await waitForTx(args.provider, tx.hash)
-  })
+    .description("Removes an admin")
+    .option('--admin <address>', 'Address of admin', constants.adminAddresses[0])
+    .option('--bridge <address>', 'Bridge contract address', constants.BRIDGE_ADDRESS)
+    .action(async function (args) {
+        await setupParentArgs(args, args.parent.parent)
+        const bridgeInstance = new ethers.Contract(args.bridge, constants.ContractABIs.Bridge.abi, args.wallet);
+        log(args, `Removing ${args.admin} as a admin.`)
+        let tx = await bridgeInstance.revokeRole(constants.ADMIN_ROLE, args.admin)
+        await waitForTx(args.provider, tx.hash)
+    })
 
 const addRelayerCmd = new Command("add-relayer")
     .description("Add a relayer")
@@ -114,7 +114,7 @@ const withdrawCmd = new Command("withdraw")
     .description("Withdraw funds collected from fees")
     .option('--bridge <address>', 'Bridge contract address', constants.BRIDGE_ADDRESS)
     .option('--handler <address>', 'Handler contract address', constants.ERC20_HANDLER_ADDRESS)
-    .option('--tokenContract <address>', 'ERC20 or ERC721 token contract address', constants.ERC20_ADDRESS)
+    .option('--tokenContract <address>', 'ERC20 token contract address', constants.ERC20_ADDRESS)
     .option('--recipient <address>', 'Address to withdraw to', constants.relayerAddresses[0])
     .option('--amountOrId <value>', 'Token ID or amount to withdraw', 1)
     .action(async function (args) {
--- cb-sol-cli/cmd/deploy.js
+++ cb-sol-cli/cmd/deploy.js
@@ -1,7 +1,7 @@
 const ethers = require('ethers');
-const {Command} = require('commander');
+const { Command } = require('commander');
 const constants = require('../constants');
-const {setupParentArgs, splitCommaList, isValidAddress} = require("./utils")
+const { setupParentArgs, splitCommaList, isValidAddress } = require("./utils")
 
 const deployCmd = new Command("deploy")
     .description("Deploys contracts via RPC")
@@ -13,31 +13,20 @@ const deployCmd = new Command("deploy")
     .option('--all', 'Deploy all contracts')
     .option('--bridge', 'Deploy bridge contract')
     .option('--erc20Handler', 'Deploy erc20Handler contract')
-    .option('--erc721Handler', 'Deploy erc721Handler contract')
-    .option('--genericHandler', 'Deploy genericHandler contract')
     .option('--bridgeAddress <address>', 'Bridge contract address for independent handler deployment', "")
     .option('--erc20', 'Deploy erc20 contract')
     .option('--erc20Symbol <symbol>', 'Name for the erc20 contract', "")
     .option('--erc20Name <name>', 'Symbol for the erc20 contract', "")
     .option('--erc20Decimals <amount>', 'Decimals for erc20 contract', 18)
-    .option('--erc721', 'Deploy erc721 contract')
-    .option('--erc721Symbol <symbol>', 'Name for the erc721 contract', "")
-    .option('--erc721Name <name>', 'Symbol for the erc721 contract', "")
-    .option('--erc721BaseUri <uri>', 'Base URI for the erc721 contract', "")
-    .option('--centAsset', 'Deploy centrifuge asset contract')
-    .option('--wetc', 'Deploy wrapped ETC Erc20 contract')
     .option('--config', 'Logs the configuration based on the deployment', false)
     .action(async (args) => {
         await setupParentArgs(args, args.parent)
         let startBal = await args.provider.getBalance(args.wallet.address)
         console.log("Deploying contracts...")
-        if(args.all) {
+        if (args.all) {
             await deployBridgeContract(args);
             await deployERC20Handler(args);
-            await deployERC721Handler(args)
-            await deployGenericHandler(args)
             await deployERC20(args)
-            await deployERC721(args)
         } else {
             let deployed = false
             if (args.bridge) {
@@ -48,31 +37,10 @@ const deployCmd = new Command("deploy")
                 await deployERC20Handler(args);
                 deployed = true
             }
-            if (args.erc721Handler) {
-                await deployERC721Handler(args)
-                deployed = true
-            }
-            if (args.genericHandler) {
-                await deployGenericHandler(args)
-                deployed = true
-            }
             if (args.erc20) {
                 await deployERC20(args)
                 deployed = true
             }
-            if (args.erc721) {
-                await deployERC721(args)
-                deployed = true
-            }
-            if (args.centAsset) {
-                await deployCentrifugeAssetStore(args);
-                deployed = true
-            }
-            if (args.wetc) {
-                await deployWETC(args)
-                deployed = true
-            }
-
             if (!deployed) {
                 throw new Error("must specify --all or specific contracts to deploy")
             }
@@ -92,8 +60,6 @@ const createConfig = (args) => {
     config.endpoint = args.url;
     config.bridge = args.bridgeAddress;
     config.erc20Handler = args.erc20HandlerContract;
-    config.erc721Handler = args.erc721HandlerContract;
-    config.genericHandler = args.genericHandlerContract;
     config.gasLimit = args.gasLimit.toNumber();
     config.maxGasPrice = args.gasPrice.toNumber();
     config.startBlock = "0"
@@ -127,17 +93,7 @@ Bridge:             ${args.bridgeAddress ? args.bridgeAddress : "Not Deployed"}
 ----------------------------------------------------------------
 Erc20 Handler:      ${args.erc20HandlerContract ? args.erc20HandlerContract : "Not Deployed"}
 ----------------------------------------------------------------
-Erc721 Handler:     ${args.erc721HandlerContract? args.erc721HandlerContract : "Not Deployed"}
-----------------------------------------------------------------
-Generic Handler:    ${args.genericHandlerContract ? args.genericHandlerContract : "Not Deployed"}
-----------------------------------------------------------------
 Erc20:              ${args.erc20Contract ? args.erc20Contract : "Not Deployed"}
-----------------------------------------------------------------
-Erc721:             ${args.erc721Contract ? args.erc721Contract : "Not Deployed"}
-----------------------------------------------------------------
-Centrifuge Asset:   ${args.centrifugeAssetStoreContract ? args.centrifugeAssetStoreContract : "Not Deployed"}
-----------------------------------------------------------------
-WETC:               ${args.WETCContract ? args.WETCContract : "Not Deployed"}
 ================================================================
         `)
 }
@@ -154,7 +110,7 @@ async function deployBridgeContract(args) {
         args.relayerThreshold,
         ethers.utils.parseEther(args.fee.toString()),
         args.expiry,
-        { gasPrice: args.gasPrice, gasLimit: args.gasLimit}
+        { gasPrice: args.gasPrice, gasLimit: args.gasLimit }
     );
     await contract.deployed();
     args.bridgeAddress = contract.address
@@ -163,7 +119,7 @@ async function deployBridgeContract(args) {
 
 async function deployERC20(args) {
     const factory = new ethers.ContractFactory(constants.ContractABIs.Erc20Mintable.abi, constants.ContractABIs.Erc20Mintable.bytecode, args.wallet);
-    const contract = await factory.deploy(args.erc20Name, args.erc20Symbol, args.erc20Decimals, { gasPrice: args.gasPrice, gasLimit: args.gasLimit});
+    const contract = await factory.deploy(args.erc20Name, args.erc20Symbol, args.erc20Decimals, { gasPrice: args.gasPrice, gasLimit: args.gasLimit });
     await contract.deployed();
     args.erc20Contract = contract.address
     console.log("✓ ERC20 contract deployed")
@@ -175,58 +131,10 @@ async function deployERC20Handler(args) {
         return
     }
     const factory = new ethers.ContractFactory(constants.ContractABIs.Erc20Handler.abi, constants.ContractABIs.Erc20Handler.bytecode, args.wallet);
-    const contract = await factory.deploy(args.bridgeAddress, [], [], [], { gasPrice: args.gasPrice, gasLimit: args.gasLimit});
+    const contract = await factory.deploy(args.bridgeAddress, [], [], [], { gasPrice: args.gasPrice, gasLimit: args.gasLimit });
     await contract.deployed();
     args.erc20HandlerContract = contract.address
     console.log("✓ ERC20Handler contract deployed")
-}
-
-async function deployERC721(args) {
-    const factory = new ethers.ContractFactory(constants.ContractABIs.Erc721Mintable.abi, constants.ContractABIs.Erc721Mintable.bytecode, args.wallet);
-    const contract = await factory.deploy(args.erc721Name, args.erc721Symbol, args.erc721BaseUri, { gasPrice: args.gasPrice, gasLimit: args.gasLimit});
-    await contract.deployed();
-    args.erc721Contract = contract.address
-    console.log("✓ ERC721 contract deployed")
-}
-
-async function deployERC721Handler(args) {
-    if (!isValidAddress(args.bridgeAddress)) {
-        console.log("ERC721Handler contract failed to deploy due to invalid bridge address")
-        return
-    }
-    const factory = new ethers.ContractFactory(constants.ContractABIs.Erc721Handler.abi, constants.ContractABIs.Erc721Handler.bytecode, args.wallet);
-    const contract = await factory.deploy(args.bridgeAddress,[],[],[], { gasPrice: args.gasPrice, gasLimit: args.gasLimit});
-    await contract.deployed();
-    args.erc721HandlerContract = contract.address
-    console.log("✓ ERC721Handler contract deployed")
-}
-
-async function deployGenericHandler(args) {
-    if (!isValidAddress(args.bridgeAddress)) {
-        console.log("GenericHandler contract failed to deploy due to invalid bridge address")
-        return
-    }
-    const factory = new ethers.ContractFactory(constants.ContractABIs.GenericHandler.abi, constants.ContractABIs.GenericHandler.bytecode, args.wallet)
-    const contract = await factory.deploy(args.bridgeAddress, [], [], [], [], { gasPrice: args.gasPrice, gasLimit: args.gasLimit})
-    await contract.deployed();
-    args.genericHandlerContract = contract.address
-    console.log("✓ GenericHandler contract deployed")
-}
-
-async function deployCentrifugeAssetStore(args) {
-    const factory = new ethers.ContractFactory(constants.ContractABIs.CentrifugeAssetStore.abi, constants.ContractABIs.CentrifugeAssetStore.bytecode, args.wallet);
-    const contract = await factory.deploy({ gasPrice: args.gasPrice, gasLimit: args.gasLimit});
-    await contract.deployed();
-    args.centrifugeAssetStoreContract = contract.address
-    console.log("✓ CentrifugeAssetStore contract deployed")
-}
-
-async function deployWETC(args) {
-    const factory = new ethers.ContractFactory(constants.ContractABIs.WETC.abi, constants.ContractABIs.WETC.bytecode, args.wallet);
-    const contract = await factory.deploy({ gasPrice: args.gasPrice, gasLimit: args.gasLimit});
-    await contract.deployed();
-    args.WETCContract = contract.address
-    console.log("✓ WETC contract deployed")
 }
 
 module.exports = deployCmd
