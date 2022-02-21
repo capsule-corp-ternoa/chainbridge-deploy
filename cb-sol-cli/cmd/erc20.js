aedd7a5126cd52ddc57f1607a4c411d11394f6d3
root <timothee.delabrouille@gmail.com>
2022-02-21 12:21:59 +0000

    changed default addresses for contract to keep up with erc721 removal

--- cb-sol-cli/cmd/erc20.js
+++ cb-sol-cli/cmd/erc20.js
@@ -1,8 +1,8 @@
 const ethers = require('ethers');
 const constants = require('../constants');
 
-const {Command} = require('commander');
-const {setupParentArgs, waitForTx, log, expandDecimals} = require("./utils")
+const { Command } = require('commander');
+const { setupParentArgs, waitForTx, log, expandDecimals } = require("./utils")
 
 const mintCmd = new Command("mint")
     .description("Mints erc20 tokens")
@@ -21,7 +21,7 @@ const addMinterCmd = new Command("add-minter")
     .description("Add a new minter to the contract")
     .option('--erc20Address <address>', 'ERC20 contract address', constants.ERC20_ADDRESS)
     .option('--minter <address>', 'Minter address', constants.relayerAddresses[1])
-    .action(async function(args) {
+    .action(async function (args) {
         await setupParentArgs(args, args.parent.parent)
         const erc20Instance = new ethers.Contract(args.erc20Address, constants.ContractABIs.Erc20Mintable.abi, args.wallet);
         let MINTER_ROLE = await erc20Instance.MINTER_ROLE();
@@ -40,7 +40,7 @@ const approveCmd = new Command("approve")
 
         const erc20Instance = new ethers.Contract(args.erc20Address, constants.ContractABIs.Erc20Mintable.abi, args.wallet);
         log(args, `Approving ${args.recipient} to spend ${args.amount} tokens from ${args.wallet.address}!`);
-        const tx = await erc20Instance.approve(args.recipient, expandDecimals(args.amount, args.parent.decimals), { gasPrice: args.gasPrice, gasLimit: args.gasLimit});
+        const tx = await erc20Instance.approve(args.recipient, expandDecimals(args.amount, args.parent.decimals), { gasPrice: args.gasPrice, gasLimit: args.gasLimit });
         await waitForTx(args.provider, tx.hash)
     })
 
@@ -59,13 +59,13 @@ const depositCmd = new Command("deposit")
         const bridgeInstance = new ethers.Contract(args.bridge, constants.ContractABIs.Bridge.abi, args.wallet);
         const data = '0x' +
             ethers.utils.hexZeroPad(ethers.utils.bigNumberify(expandDecimals(args.amount, args.parent.decimals)).toHexString(), 32).substr(2) +    // Deposit Amount        (32 bytes)
-            ethers.utils.hexZeroPad(ethers.utils.hexlify((args.recipient.length - 2)/2), 32).substr(2) +    // len(recipientAddress) (32 bytes)
+            ethers.utils.hexZeroPad(ethers.utils.hexlify((args.recipient.length - 2) / 2), 32).substr(2) +    // len(recipientAddress) (32 bytes)
             args.recipient.substr(2);                    // recipientAddress      (?? bytes)
 
         log(args, `Constructed deposit:`)
         log(args, `  Resource Id: ${args.resourceId}`)
         log(args, `  Amount: ${expandDecimals(args.amount, args.parent.decimals).toHexString()}`)
-        log(args, `  len(recipient): ${(args.recipient.length - 2)/ 2}`)
+        log(args, `  len(recipient): ${(args.recipient.length - 2) / 2}`)
         log(args, `  Recipient: ${args.recipient}`)
         log(args, `  Raw: ${data}`)
         log(args, `Creating deposit to initiate transfer!`);
@@ -75,7 +75,7 @@ const depositCmd = new Command("deposit")
             args.dest, // destination chain id
             args.resourceId,
             data,
-            { gasPrice: args.gasPrice, gasLimit: args.gasLimit}
+            { gasPrice: args.gasPrice, gasLimit: args.gasLimit }
         );
 
         await waitForTx(args.provider, tx.hash)
@@ -85,13 +85,13 @@ const balanceCmd = new Command("balance")
     .description("Get the balance for an account")
     .option('--address <address>', 'Address to query', constants.deployerAddress)
     .option('--erc20Address <address>', 'ERC20 contract address', constants.ERC20_ADDRESS)
-    .action(async function(args) {
+    .action(async function (args) {
         await setupParentArgs(args, args.parent.parent)
 
         const erc20Instance = new ethers.Contract(args.erc20Address, constants.ContractABIs.Erc20Mintable.abi, args.wallet);
         const balance = await erc20Instance.balanceOf(args.address)
         const decimals = await erc20Instance.decimals();
-        log(args, `Account ${args.address} has a balance of ${ethers.utils.formatUnits(balance, decimals)}` )
+        log(args, `Account ${args.address} has a balance of ${ethers.utils.formatUnits(balance, decimals)}`)
     })
 
 const allowanceCmd = new Command("allowance")
@@ -99,7 +99,7 @@ const allowanceCmd = new Command("allowance")
     .option('--spender <address>', 'Address of spender', constants.ERC20_HANDLER_ADDRESS)
     .option('--owner <address>', 'Address of token owner', constants.deployerAddress)
     .option('--erc20Address <address>', 'ERC20 contract address', constants.ERC20_ADDRESS)
-    .action(async function(args) {
+    .action(async function (args) {
         await setupParentArgs(args, args.parent.parent)
 
         const erc20Instance = new ethers.Contract(args.erc20Address, constants.ContractABIs.Erc20Mintable.abi, args.wallet);
@@ -112,25 +112,25 @@ const wetcDepositCmd = new Command("wetc-deposit")
     .description("Deposit ether into a wetc contract to mint tokens")
     .option('--amount <number>', 'Amount of ether to include in the deposit')
     .option('--wetcAddress <address>', 'ERC20 contract address', constants.WETC_ADDRESS)
-    .action(async function(args) {
-            await setupParentArgs(args, args.parent.parent)
+    .action(async function (args) {
+        await setupParentArgs(args, args.parent.parent)
 
-            const wetcInstance = new ethers.Contract(args.wetcAddress, constants.ContractABIs.WETC.abi, args.wallet);
-            let tx = await wetcInstance.deposit({value: ethers.utils.parseEther(args.amount), gasPrice: args.gasPrice, gasLimit: args.gasLimit})
-            await waitForTx(args.provider, tx.hash)
-            const newBalance = await wetcInstance.balanceOf(args.wallet.address)
-            const decimals = await wetcInstance.decimals();
-            log(args, `Deposited ${args.amount} into ${args.wetcAddress}. New Balance: ${ethers.utils.formatUnits(newBalance, decimals)}`)
+        const wetcInstance = new ethers.Contract(args.wetcAddress, constants.ContractABIs.WETC.abi, args.wallet);
+        let tx = await wetcInstance.deposit({ value: ethers.utils.parseEther(args.amount), gasPrice: args.gasPrice, gasLimit: args.gasLimit })
+        await waitForTx(args.provider, tx.hash)
+        const newBalance = await wetcInstance.balanceOf(args.wallet.address)
+        const decimals = await wetcInstance.decimals();
+        log(args, `Deposited ${args.amount} into ${args.wetcAddress}. New Balance: ${ethers.utils.formatUnits(newBalance, decimals)}`)
     })
 
 const createErc20ProposalData = (amount, recipient, decimals) => {
-        if (recipient.substr(0, 2) === "0x") {
-                recipient = recipient.substr(2)
-        }
-        return '0x' +
-            ethers.utils.hexZeroPad(ethers.utils.hexlify(amount), 32).substr(2) +
-            ethers.utils.hexZeroPad(ethers.utils.hexlify(recipient.length / 2 + recipient.length % 2), 32).substr(2) +
-            recipient;
+    if (recipient.substr(0, 2) === "0x") {
+        recipient = recipient.substr(2)
+    }
+    return '0x' +
+        ethers.utils.hexZeroPad(ethers.utils.hexlify(amount), 32).substr(2) +
+        ethers.utils.hexZeroPad(ethers.utils.hexlify(recipient.length / 2 + recipient.length % 2), 32).substr(2) +
+        recipient;
 }
 
 const proposalDataHashCmd = new Command("data-hash")
@@ -138,7 +138,7 @@ const proposalDataHashCmd = new Command("data-hash")
     .option('--amount <value>', "Amount to transfer", 1)
     .option('--recipient <address>', 'Destination recipient address', constants.relayerAddresses[4])
     .option('--handler <address>', 'ERC20 handler  address', constants.ERC20_HANDLER_ADDRESS)
-    .action(async function(args) {
+    .action(async function (args) {
         const data = createErc20ProposalData(expandDecimals(args.amount, args.parent.decimals), args.recipient)
         const hash = ethers.utils.solidityKeccak256(["address", "bytes"], [args.handler, data])
 
@@ -146,7 +146,7 @@ const proposalDataHashCmd = new Command("data-hash")
     })
 
 const erc20Cmd = new Command("erc20")
-.option('-d, decimals <number>', "The number of decimal places for the erc20 token", 18)
+    .option('-d, decimals <number>', "The number of decimal places for the erc20 token", 18)
 
 erc20Cmd.addCommand(mintCmd)
 erc20Cmd.addCommand(addMinterCmd)
@@ -154,7 +154,6 @@ erc20Cmd.addCommand(approveCmd)
 erc20Cmd.addCommand(depositCmd)
 erc20Cmd.addCommand(balanceCmd)
 erc20Cmd.addCommand(allowanceCmd)
-erc20Cmd.addCommand(wetcDepositCmd)
 erc20Cmd.addCommand(proposalDataHashCmd)
 
 module.exports = erc20Cmd
